63fcb2f35063451bca2fcfa5b91361f4988bc13e
phillipeastwood <1093482742@qq.com>
2019-04-09 00:03:55 +0800

    nothing change

--- spring-boot-data-rest/src/main/java/com/os/china/RunApplication.java
+++ spring-boot-data-rest/src/main/java/com/os/china/RunApplication.java
@@ -12,6 +12,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RunApplication {
 
+	/**
+	 * main
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(RunApplication.class, args);
 	}
